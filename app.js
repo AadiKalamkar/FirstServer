a1dfa13b6ff8336a2fadf543df0c0b22ffab2c82
Aditya Kalamkar <adityaklmkr@gmail.com>
2024-06-19 09:49:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import "dotenv/config";
+import db from "./db/db.js";
+import router from "./routes/index.js";
+
+const app = express();
+const port = process.env.PORT;
+
+app.set("view engine", "ejs");
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", router);
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
