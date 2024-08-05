487fbee09c92060e9746685b17070cee9bf425a6
Aditya Kalamkar <adityaklmkr@gmail.com>
2024-08-05 09:30:16 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const mongourl = process.env.MONGOURL
-const db = mongoose.connect("mongodb+srv://adityaklmkr:RuY3ZAIlMqVfj9aY@cluster0.kkdvuft.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test").then(()=>{
+const db = mongoose.connect(mongourl).then(()=>{
     console.log("database connected");
 }).catch((err)=>{
     console.log(err)
